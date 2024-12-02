ac6793f9532736cea11337d995c589348bd2c899
Spyros Magliveras <ssmag@github.com>
2024-12-02 13:53:25 -0700

    Move file read class to Util

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -19,17 +19,43 @@ object Day1 {
                 }
         }
 
-        list1Arr.sort()
-        list2Arr.sort()
+        print(getSimilarityScore(list1Arr, list2Arr))
+        getDistanceOfArrays(list1Arr.sorted(), list2Arr.sorted())
+    }
 
-        val distanceArr = list1Arr.zip(list2Arr) { i, j -> abs(i - j)}
+    private fun getSimilarityScore(arr1: List<Int>, arr2: List<Int>): Int {
+        val map1 = mutableMapOf<Int, Int>()
+        val map2 = mutableMapOf<Int, Int>()
+        val combinedMap = mutableMapOf<Int, Int>()
+
+        arr1.forEach { num ->
+            map1[num] = map1[num]?.plus(1) ?: 1
+        }
+        arr2.forEach {  num ->
+            map2[num] = map2[num]?.plus(1) ?: 1
+        }
+
+        map1.forEach { (k, _) ->
+            combinedMap[k] = (map1[k] ?: 0) * (map2[k]?: 0)
+        }
+
+        val result = combinedMap.entries.fold(0) { acc, entry ->
+           acc + (entry.key * entry.value)
+        }
+
+        return result
+    }
+
+    private fun getDistanceOfArrays(arr1: List<Int>, arr2: List<Int>): Int {
+        val distanceArr = arr1.zip(arr2) { i, j -> abs(i - j)}
         val sum = distanceArr.fold(0) { acc, i -> acc + i }
 
-        println("sum: $sum")
+        return sum
     }
 
 }
 
 
 const val FILENAME = "src/main/res/day1.txt"
+const val EXAMPLE_FILENAME = "src/main/res/example-day1.txt"
 const val DELIM = "   "
