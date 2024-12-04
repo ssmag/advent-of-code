a53c5b2f7629a6353cf4c86f4147622b40c3547f
Spyros Magliveras <ssmag@github.com>
2024-12-03 19:25:53 -0700

    Solve day 3 part 2

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -6,8 +6,44 @@ object Day3 {
     fun main(args: Array<String>) {
         val lineList = getLinesList(FILENAME)
         val fullLine = lineList.reduce { acc, s -> "$acc$s"}
+        getSumOfProductsWhenEnabled(fullLine)
+    }
+
+    private fun getSumOfProductsWhenEnabled(s: String) {
+        val regex = Regex("mul\\([0-9]+,[0-9]+\\)|do\\(\\)|don\'t\\(\\)")
+        val multiplications = regex.findAll(s).map { mult -> mult.value }
+        var sum = 0
+        var shouldMul = true
+        multiplications.forEach { operation ->
+            if (operation == DO) {
+                shouldMul = true
+                return@forEach
+            }
+            if (operation == DONT) {
+                shouldMul = false
+                return@forEach
+            }
+            if (!shouldMul) {
+                return@forEach
+            }
+            val product = operation
+                .filterNot { c ->
+                    c == ')' || c == '(' || (c in 'a'..'z')
+                }
+                .split(DELIM)
+                .map { numString ->
+                    Integer.parseInt(numString)
+                }
+                .reduce { acc, i -> acc * i }
+            sum += product
+        }
+        println(sum)
+
+    }
+
+    private fun getSumOfProductsOfMulOperations(s :String) {
         val regex = Regex("mul\\([0-9]+,[0-9]+\\)")
-        val multiplications = regex.findAll(fullLine)
+        val multiplications = regex.findAll(s)
         var sum = 0
         multiplications.forEach { multiplication ->
             val operation = multiplication.value
@@ -28,5 +64,7 @@ object Day3 {
 }
 
 
+private const val DO = "do()"
+private const val DONT = "don't()"
 private const val FILENAME = "src/main/res/day3.txt"
 private const val DELIM = ","
