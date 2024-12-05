32ff28dcf99815294d5c3185ad02783ec45634da
Spyros Magliveras <ssmag@github.com>
2024-12-04 17:50:55 -0700

    Count XMas occurrences

--- src/main/kotlin/Day4.kt
+++ src/main/kotlin/Day4.kt
@@ -1,51 +1,81 @@
 import kotlin.system.measureTimeMillis
 
 object Day4 {
+    private val charTable = mutableMapOf<Char, MutableList<Pair<Int, Int>>>(
+        'X' to mutableListOf(),
+        'M' to mutableListOf(),
+        'A' to mutableListOf(),
+        'S' to mutableListOf()
+    )
+
+    private val crossArr = listOf('M', 'S')
+
+    private lateinit var matrix: Array<Array<Char>>
+    private lateinit var chosenMatrix: Array<Array<Char>>
+
     @JvmStatic
     fun main(args: Array<String>) {
-        val lineList = Util.getLinesList(EXAMPLE_FILENAME)
-        var ceresNum = -1
+        val lineList = Util.getLinesList(FILENAME)
+        var ceresNum: Int
+        matrix = Array(lineList.size) { Array(lineList.first().length) { '.' } }
+        chosenMatrix = Array(lineList.size) { Array(lineList.first().length) { '.' } }
+        passCharactersToMatrix(lineList)
+
+        findCeresNumber()
         val time = measureTimeMillis {
-            ceresNum = findCeresNumber(lineList)
+            countXMas()
         }
         println("time elapsed: $time")
-        println(ceresNum)
+
+        println(countXMas())
     }
 
-    private fun findCeresNumber(lineList: MutableList<String>): Int {
-        val matrix = Array(lineList.size) { Array(lineList.first().length) { '.' } }
-        val charTable = mutableMapOf<Char, MutableList<Pair<Int, Int>>>(
-            'X' to mutableListOf(),
-            'M' to mutableListOf(),
-            'A' to mutableListOf(),
-            'S' to mutableListOf()
-        )
+    private fun countXMas(): Int {
+        var res = 0
+        charTable['A']?.forEach { aCoords ->
+            //take top left
+            val topLeftCords = aCoords.copy(aCoords.first - 1, aCoords.second - 1)
+            val bottomRightCords = aCoords.copy(aCoords.first + 1, aCoords.second + 1)
+            val topRightCords = aCoords.copy(aCoords.first - 1, aCoords.second + 1)
+            val bottomLeftCords = aCoords.copy(aCoords.first + 1, aCoords.second - 1)
+            try {
+                val topLeftChar = matrix.getCords(topLeftCords)
+                val bottomRightChar = matrix.getCords(bottomRightCords)
+                val topRightChar = matrix.getCords(topRightCords)
+                val bottomLeftChar = matrix.getCords(bottomLeftCords)
 
-        lineList.forEachIndexed { i, line ->
-            line.forEachIndexed { j, c ->
-                when (c) {
-                    'X', 'M', 'A', 'S' -> {
-                        charTable[c]!!.add(Pair(i, j))
-                    }
+                if (crossArr.contains(topLeftChar) &&
+                    crossArr.indexOf(bottomRightChar) == ((crossArr.indexOf(topLeftChar) + 1) % 2) &&
+                    crossArr.contains(topRightChar) &&
+                    crossArr.indexOf(bottomLeftChar) == ((crossArr.indexOf(topRightChar) + 1) % 2)
+                ) {
+                    res++
                 }
-                matrix[i][j] = c
+
+            } catch (e: ArrayIndexOutOfBoundsException) {
+                // do nothing
             }
         }
 
+        return res
+
+    }
+
+    private fun findCeresNumber(): Int {
         return charTable['X']
-        ?.map { xCoords ->
-            findNearbyLetters(charTable, xCoords, 'M')
-        }
-        ?.map { mCoordsWithDir ->
-            findNearByLettersWithDirection(charTable, mCoordsWithDir, 'A')
-        }
-        ?.map { aCoordsWithDir ->
-            findNearByLettersWithDirection(charTable, aCoordsWithDir, 'S')
-        }
-        ?.flatMap { sLettersWithDir ->
-            sLettersWithDir.values
-        }
-        ?.count() ?: -1
+            ?.map { xCoords ->
+                findNearbyLetters(charTable, xCoords, 'M')
+            }
+            ?.map { mCoordsWithDir ->
+                findNearByLettersWithDirection(charTable, mCoordsWithDir, 'A')
+            }
+            ?.map { aCoordsWithDir ->
+                findNearByLettersWithDirection(charTable, aCoordsWithDir, 'S')
+            }
+            ?.flatMap { sLettersWithDir ->
+                sLettersWithDir.values
+            }
+            ?.count() ?: -1
     }
 
     private fun findNearByLettersWithDirection(
@@ -58,9 +88,9 @@ object Day4 {
         roots.forEach { rootMap ->
             val root = rootMap.value
             val nearbyLettersByDir = findNearbyLetters(charTable, root, c)
-            .filter { nearbyLetter ->
-                nearbyLetter.key == rootMap.key
-            }
+                .filter { nearbyLetter ->
+                    nearbyLetter.key == rootMap.key
+                }
             result.putAll(nearbyLettersByDir)
         }
 
@@ -71,18 +101,18 @@ object Day4 {
         charTable: MutableMap<Char, MutableList<Pair<Int, Int>>>,
         root: Pair<Int, Int>,
         c: Char,
-    ): Map<Direction, Pair<Int,Int>> {
+    ): Map<Direction, Pair<Int, Int>> {
         val x = root.first
         val y = root.second
         val allDirs = mapOf(
-           Direction.TOP_LEFT       to Pair(x-1,   y-1),
-           Direction.TOP_CENTER     to Pair(x-1,   y),
-           Direction.TOP_RIGHT      to Pair(x-1,   y+1),
-           Direction.CENTER_LEFT    to Pair(x,     y-1),
-           Direction.CENTER_RIGHT   to Pair(x,     y+1),
-           Direction.BOTTOM_LEFT    to Pair(x+1,   y-1),
-           Direction.BOTTOM_CENTER  to Pair(x+1,   y),
-           Direction.BOTTOM_RIGHT   to Pair(x+1,   y+1),
+            Direction.TOP_LEFT to Pair(x - 1, y - 1),
+            Direction.TOP_CENTER to Pair(x - 1, y),
+            Direction.TOP_RIGHT to Pair(x - 1, y + 1),
+            Direction.CENTER_LEFT to Pair(x, y - 1),
+            Direction.CENTER_RIGHT to Pair(x, y + 1),
+            Direction.BOTTOM_LEFT to Pair(x + 1, y - 1),
+            Direction.BOTTOM_CENTER to Pair(x + 1, y),
+            Direction.BOTTOM_RIGHT to Pair(x + 1, y + 1),
         )
 
         return allDirs
@@ -91,8 +121,12 @@ object Day4 {
             }
     }
 
+    private fun <T> Array<Array<T>>.getCords(pair: Pair<Int, Int>): T {
+        return this[pair.first][pair.second]
+    }
 
-    private enum class Direction {
+
+    enum class Direction {
         TOP_LEFT,
         TOP_CENTER,
         TOP_RIGHT,
@@ -103,7 +137,20 @@ object Day4 {
         BOTTOM_RIGHT,
     }
 
+    private fun passCharactersToMatrix(lineList: MutableList<String>) {
+        lineList.forEachIndexed { i, line ->
+            line.forEachIndexed { j, c ->
+                when (c) {
+                    'X', 'M', 'A', 'S' -> {
+                        charTable[c]!!.add(Pair(i, j))
+                    }
+                }
+                matrix[i][j] = c
+            }
+        }
+
+    }
+
     private const val FILENAME = "src/main/res/day4.txt"
     private const val EXAMPLE_FILENAME = "src/main/res/example-day4.txt"
-    private const val DELIM = ","
 }
