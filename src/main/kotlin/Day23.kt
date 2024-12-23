ccb5d967642f3bb75c2bb25f9160a34395050120
Spyros Magliveras <ssmag@github.com>
2024-12-23 18:34:17 -0500

    Setup day 23

---
+++ src/main/kotlin/Day23.kt
@@ -0,0 +1,27 @@
+import kotlin.system.measureTimeMillis
+
+object Day23 {
+
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val time = measureTimeMillis {
+        }
+
+        println(time)
+    }
+
+    private fun firstStar(): Long {
+        return -1
+    }
+
+    private fun secondStar(): Long {
+        return -1
+    }
+
+
+    private const val FILENAME = "src/main/res/day23.txt"
+    private const val EXAMPLE_FILENAME = "src/main/res/example-day23.txt"
+    private val DELIM = "\n\n"
+
+}
